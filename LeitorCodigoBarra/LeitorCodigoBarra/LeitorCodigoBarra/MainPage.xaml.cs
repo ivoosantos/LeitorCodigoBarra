33ef840a72220d0c0180ae79927e824051bff5b6
ivoosantos <oliveiradossantosivo@gmail.com>
2022-02-20 13:21:42 -0300

    Iniciando projeto já com os testes

---
+++ LeitorCodigoBarra/LeitorCodigoBarra/LeitorCodigoBarra/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace LeitorCodigoBarra
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushModalAsync(scan);
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopModalAsync(true);
+                    mycode.Text = result.Text;
+                });
+            };
+
+        }
+    }
+}
